023789cdf71e2b0734a72a2d209ee59530d1b845
daniela marques <dd978452@gmail.com>
2023-01-13 09:47:37 -0300

    html + variaveis de JavaScript

---
+++ projeto.js
@@ -0,0 +1,8 @@
+let nomeCliente = prompt ("Qual é o seu nome?") //let= variavel que não pode ser repetida
+let quantidade = prompt ("Qual a quantidade de TVs desejada?")
+
+const valor = 2.739 //const= variavel que tem um valor fixo, que não pode ser modificada
+
+var resultado = quantidade*valor
+
+
